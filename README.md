3689d982239e6fc98eb0b37d8f385075417d1ba7
asingjr2 <asingjr2@gmail.com>
2019-09-30 20:37:20 -0400

    updated readME and added small demo for functools.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-"# PYTHON_STD_LIB" 
+## PYTHON_STD_LIB
+
+#### Contents
+- unit_testing examples
---
+++ functools_and_hashattr/base.py
@@ -0,0 +1,45 @@
+"""Creates base class for use with functools."""
+from functools import wraps
+
+
+def print_through_functools(f):
+    @wraps(f)
+    def function_wrapper(*args, **kwargs):
+        if kwargs.get('exclude'):
+            print('not excuting sum_ function since kwargs is missing')
+            return function_wrapper
+        else:
+            return f(*args, **kwargs)
+
+    return function_wrapper
+
+@print_through_functools
+def sum_(*args,**kwargs):
+    print('excuting sum_ function since kwargs is included')
+    total = 0
+    for number in args:
+        total = number + total
+        print(total)
+    
+    return total
+
+class MathThing():
+    def __init__(self, age=None):
+        self.first = 'first_name'
+        self.last = 'last_name'
+
+        if age:
+            self.age = age
+
+
+sum_(10,100)
+sum_(10,500, exclude=True)
+
+print('\nchecking hashattr booleans')
+name = MathThing()
+name2 = MathThing(90)
+# checking for age attribute in object
+check = hasattr(name, 'age')
+print(check)
+check2 = hasattr(name2, 'age')
+print(check2)
